b5ba493387d4e823a9214f9d3ac9c31f0df6c8e9
waleedahmad98 <w.ahmad1024@gmail.com>
2021-01-08 23:23:30 +0500

    added main.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command(name='barry')
+async def barry(ctx):
+    await ctx.send('BORK!')
+
+bot.run(TOKEN)
